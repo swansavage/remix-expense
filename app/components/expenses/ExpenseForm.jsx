0dc67ebb9016b096b3d231d3c7655d70bddedc18
swansavage@github.com <swansavage@gmail.com>
2023-09-04 16:56:31 -0500

    added user feedback/disabled buttong while saving expense

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,12 +1,16 @@
-import { Link, useActionData } from '@remix-run/react';
+import { Link, Form, useActionData, useNavigation } from '@remix-run/react';
 
 function ExpenseForm() {
 	const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
 
 	const validationErrors = useActionData();
 
+	const navigation = useNavigation();
+
+	const isSubmitting = navigation.state !== 'idle';
+
 	return (
-		<form method="post" className="form" id="expense-form">
+		<Form method="post" className="form" id="expense-form">
 			<p>
 				<label htmlFor="title">Expense Title</label>
 				<input type="text" id="title" name="title" required maxLength={30} />
@@ -37,10 +41,12 @@ function ExpenseForm() {
 				</ul>
 			)}
 			<div className="form-actions">
-				<button>Save Expense</button>
+				<button disabled={isSubmitting}>
+					{isSubmitting ? 'Saving...' : 'Save Expense'}
+				</button>
 				<Link to="..">Cancel</Link>
 			</div>
-		</form>
+		</Form>
 	);
 }
 
